76304fb576281c5e748a3e196add6da6a7917d4e
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2026-08-04 11:42:03 +0500

    Напишите функцию, которая переворачивает строку. Входная строка задана в виде массива символов s.

---
+++ LeetCode/reverse-string.py
@@ -0,0 +1,26 @@
+#Напишите функцию, которая переворачивает строку. Входная строка задана в виде массива символов s.
+# Это необходимо сделать, изменяя входной массив на месте с использованием дополнительной памяти O(1).
+
+
+class Solution:
+    def reverseString(self, s: list[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        
+
+        for index in range(len(s) // 2):
+            s[index], s[-index-1] = s[-index-1], s[index]
+            print(index, s)
+
+example = Solution()
+
+s = ["h","e","l","l","o"]
+example.reverseString(s)
+assert s == ["o","l","l","e","h"]
+
+s = ["H","a","n","n","a","h"]
+example.reverseString(s)
+assert s == ["h","a","n","n","a","H"]
+
+  
